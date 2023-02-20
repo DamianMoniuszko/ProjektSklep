1e7ec6dd3f4d108a29afa716315f22c06ed2018f
DamianMoniuszko <damian.moniuszko@infotech.edu.pl>
2023-02-20 08:23:31 +0100

    Pierwszy commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require_once"systemClass.php";
+require_once"layoutClass.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+SystemClass::printHead("./styles/main.css");
+?>
+<body>
+    <?php
+    LayoutClass::printHeader();
+    ?>
+
+    <section class='hero'>
+        <div class='hero__container'>
+            <h1>Welcome to DAMIANEX</h1>
+            <!-- <p>Sklep</p> -->
+            <button type='button'>
+                <a>Sklep</a>
+        </div>  
+    </section>
+
+    <?php
+    LayoutClass::printFooter();
+    ?>
+
+</body>
+</html>
---
+++ layoutClass.php
@@ -0,0 +1,49 @@
+<?php
+
+class LayoutClass {
+    static function printHeader() {
+        $conditionReander = "";
+        if(isset($_SESSION['signedIn']) && $_SESSION['signedIn'] == true) {
+            $conditionReander = "
+                <li>
+                    <a href='logout.php'>wyloguj</a>
+                </li>
+            ";
+        } else {
+            $conditionReander = "
+                <li><a href=signInPage.php>Zaloguj się</a></li>
+                <li><a href=signUnPage.php>Zarejestruj się</a></li>
+            ";
+        }
+
+        echo "
+        <header>
+        <div class='header__container'>
+            <h4>DAMIANEX</h4>
+            <nav>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>About Us</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        </header>
+        ";
+    }
+    static function printFooter() {
+        echo'
+            <footer>
+                <div class="footerconteiner">
+                    <div class="footerconteiner--column">
+                        <h4></h4>
+                        <ul>
+                            <li>Sklep Damiana Moniuszko</li>
+                            <li>Kontakt: damian@damian.pl</li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>  
+        ';
+    }
+}
